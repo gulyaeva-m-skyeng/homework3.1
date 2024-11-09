8d51a69fb5d931048babf9cb64b8c33002d06d36
gulyaeva-m-skyeng <marucosmos0924@gmail.com>
2024-11-10 01:01:20 +0300

    задание 3 часть 2

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,25 @@ public class Main {
         System.out.println("Task 1");
 
         int os = 1;
-        if (os ==0){
+        if (os == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (os ==1) {
+        } else if (os == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
         } else {
             System.out.println("Неверное значение");
+        }
+//task 2
+        System.out.println("Task 2");
+        os = 0;
+        int clientDeviceYear = 2015;
+        if (os == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+    } else if (os == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите обычную версию приложения для iOS по ссылке");
+        } else if (os == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите обычную версию приложения для Android по ссылке");
         }
 
     }
