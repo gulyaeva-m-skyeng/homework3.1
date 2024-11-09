7f0860589aa4f247bf8b7257960064dd9a3cd8d4
gulyaeva-m-skyeng <marucosmos0924@gmail.com>
2024-11-10 00:39:45 +0300

    задание 3 часть 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //task 1
+        System.out.println("Task 1");
+
+        int os = 1;
+        if (os ==0){
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (os ==1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Неверное значение");
+        }
+
     }
 }
